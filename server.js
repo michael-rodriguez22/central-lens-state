bc90deed04284e28d3ed8148d8b937dc3f4fd5ec
Michael Rodriguez <mikeyrod22@gmail.com>
2021-04-30 08:01:15 -0500

    added html routes to routes directory

---
+++ server.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server now listening on port ${PORT}`);
+});

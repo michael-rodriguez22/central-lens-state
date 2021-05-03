7f23f65e615cb2010da39d7d01aaa3f5d35484bc
Michael Rodriguez <mikeyrod22@gmail.com>
2021-05-02 21:16:55 -0500

    added nodemon and implemented initial delete functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { createNote } = require('../lib/notes');
+const { createNote, removeNote } = require('../lib/notes');
 const { notes } = require('../Data/db.json');
 
 router.get('/notes', (req, res) => {
@@ -9,6 +9,10 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const note = createNote(req.body, notes);
     res.json(note);
-})
+});
+
+router.delete('/notes/:id', (req, res) => {
+    res.json(removeNote(req.params.id, notes));
+});
 
 module.exports = router;
